46db8d45d23600716ff4912ff98b775e704f5d91
Babak Mozaffari <bmozaffa@redhat.com>
2022-12-19 12:09:29 -0800

    Taking advantage of the new Promise-based pattern to display results of the processing on the web page
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

--- listener.js
+++ listener.js
@@ -13,8 +13,48 @@ const requestListener = function (req, res) {
   var token = params.get("token");
   if (sid && ssid && hsid && token) {
     var cookie = alerts.get_secure_token(sid, ssid, hsid);
-    sync.sync(cookie, token);
-    res.end("Will create, update, or delete Google Alerts based on Airtable records");
+    sync.sync(cookie, token).then(operations => {
+      const failures = operations.addFailed.length + operations.removeFailed.length + operations.updateFailed.length;
+      if (failures > 0) {
+        res.write("Failed " + failures + " operations!");
+        res.write("<br/>")
+        for (addFail of operations.addFailed) {
+          res.write(addFail);
+          res.write("<br/>")
+        }
+        for (removeFail of operations.removeFailed) {
+          res.write(removeFail);
+          res.write("<br/>")
+        }
+        for (updateFail of operations.updateFailed) {
+          res.write(updateFail);
+          res.write("<br/>")
+        }
+      }
+      if ((failures + operations.added.length + operations.removed.length + operations.updated.length) === 0) {
+        res.write("Airtable and Google Alerts are in sync. There is nothing to do!");
+        res.write("<br/>")
+      }
+      for (record of operations.added) {
+        res.write("Added new alert with RSS Feed url " + record.rss);
+        res.write("<br/>")
+      }
+      for (record of operations.removed) {
+        res.write("Delete obsolete alert  " + record);
+        res.write("<br/>")
+      }
+      for (record of operations.updated) {
+        res.write("Updated RSS feed in Airtable for " + record.get("Alerts Keyword"));
+        res.write("<br/>")
+      }
+      res.write("<p/>")
+      res.end("Finished reconciling table with Google Alerts")
+    }).catch(function (err) {
+      console.error(err, err.stack);
+      res.write("Error processing changes!");
+      res.write("<br/>")
+      res.end(err);
+    });
   } else {
     const html = fs.readFileSync("./form.html");
     res.end(html);
