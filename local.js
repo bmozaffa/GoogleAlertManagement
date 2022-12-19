c5fe7d0bc4920e75c0191ed1ece7e28272bf2c68
Babak Mozaffari <bmozaffa@redhat.com>
2022-12-19 12:15:00 -0800

    Refactored local.js to use logging for both hardcoded and chrome cookies
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

--- local.js
+++ local.js
@@ -1,34 +1,38 @@
 const sync = require("./sync");
 const alerts = require("./googlealerts");
 
+function doSync(token) {
+  sync.sync(token, get_airtable_token()).then(operations => {
+    const failures = operations.addFailed.length + operations.removeFailed.length + operations.updateFailed.length;
+    if (failures > 0 ){
+      console.error("Failed " + failures + " operations!");
+      for (addFail of operations.addFailed) {
+        console.error(addFail);
+      }
+      for (removeFail of operations.removeFailed) {
+        console.error(removeFail);
+      }
+      for (updateFail of operations.updateFailed) {
+        console.error(updateFail);
+      }
+    }
+    for (record of operations.added) {
+      console.log("Added new alert with RSS Feed url " + record.rss);
+    }
+    for (record of operations.removed) {
+      console.log("Delete obsolete alert  " + record);
+    }
+    for (record of operations.updated) {
+      console.log("Updated RSS feed in Airtable for " + record.get("Alerts Keyword"));
+    }
+  });
+}
+
 var token = alerts.get_secure_token("RAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.", "A_xxxxxxxxxxxxxxx", "Axxxxxxxxxxxxxxxx");
-sync.sync(token, get_airtable_token()).then(operations => {
-  const failures = operations.addFailed.length + operations.removeFailed.length + operations.updateFailed.length;
-  if (failures > 0 ){
-    console.error("Failed " + failures + " operations!");
-    for (addFail of operations.addFailed) {
-      console.error(addFail);
-    }
-    for (removeFail of operations.removeFailed) {
-      console.error(removeFail);
-    }
-    for (updateFail of operations.updateFailed) {
-      console.error(updateFail);
-    }
-  }
-  for (record of operations.added) {
-    console.log("Added new alert with RSS Feed url " + record.rss);
-  }
-  for (record of operations.removed) {
-    console.log("Delete obsolete alert  " + record);
-  }
-  for (record of operations.updated) {
-    console.log("Updated RSS feed in Airtable for " + record.get("Alerts Keyword"));
-  }
-});
+doSync(token);
 
 // get_chrome_cookie().then(cookies => {
-//   sync.sync(alerts.get_secure_token(cookies.SID, cookies.SSID, cookies.HSID), get_airtable_token());
+//   doSync(alerts.get_secure_token(cookies.SID, cookies.SSID, cookies.HSID));
 // });
 //
 // async function get_chrome_cookie() {
