e2dfb5101c50f157f10912e1850149a317b379de
Babak Mozaffari <bmozaffa@redhat.com>
2022-12-18 18:48:22 -0800

    When not provided with request parameters, use a web form to collect them
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

--- listener.js
+++ listener.js
@@ -1,22 +1,23 @@
 const http = require("http");
 const {URL} = require('url');
-const host = "0.0.0.0";
+const fs = require('fs');
+const sync = require("./sync");
+const alerts = require("./googlealerts");
 
 const requestListener = function (req, res) {
   const params = new URL(`http://${req.headers.host}${req.url}`).searchParams;
-  res.writeHead(200);
+  res.writeHeader(200, {"Content-Type": "text/html"});
   var sid = params.get("sid");
   var ssid = params.get("ssid");
   var hsid = params.get("hsid");
   var token = params.get("token");
   if (sid && ssid && hsid && token) {
-    const alerts = require("./googlealerts");
     var cookie = alerts.get_secure_token(sid, ssid, hsid);
-    const sync = require("./sync");
     sync.sync(cookie, token);
     res.end("Will create, update, or delete Google Alerts based on Airtable records");
   } else {
-    res.end("Did not receive sid, ssid, and hsid parameters so cannot authenticate against google alerts and/or Airtable");
+    const html = fs.readFileSync("./form.html");
+    res.end(html);
   }
 };
 
